b2a19dd4b742b5eff780e6e661fcfa6b61a7803e
Goosa2025 <gus21002@byui.edu>
2024-12-20 04:01:46 -0700

    Coming soon splash complete

---
+++ script.js
@@ -0,0 +1,15 @@
+// const text = document.getElementById('constructionText');
+
+// text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+
+// const ele = document.querySelectorAll('span');
+
+// for (let i = 0; i < ele.length; i++) {
+//     ele[i].style.transform = "rotate(" + i*16.5 + 
+//     "deg)";
+// }
+
+const text = document.querySelector('.constructionText p');
+text.innerHTML = text.innerText.split("").map(
+  (char, i) => `<span style="transform: rotate(${i * 7.7}deg)">${char}</span>`
+).join('');
