4dc6543f8a42280e3adfaa86b8b7ee113073a222
Goosa2025 <gus21002@byui.edu>
2025-01-09 14:04:50 -0700

    Mobile books media view and java done

--- bookScript.js
+++ bookScript.js
@@ -11,7 +11,8 @@ const books = [
               to his wise and loving parents. Together, they help him understand that he is more than just a bear - He is
               a cherished child of God.`,
     link: 'Coming TBD',
-    pic: '/assets/images/placeholder-transparent.png'
+    pic: '/assets/images/bookcovers/WhoAmIReally.png',
+    release: 'January 31, 2025'
   },
   {
     title: 'The Family Investigation',
@@ -19,7 +20,8 @@ const books = [
             of family life. One day, they stumble upon an inspiring document on their wall, 'A Proclamation to the World', which
             outlines the ideals of a perfect family. Curious and eager, they begin to measure their family against these lofty standards.`,
     link: 'Coming TBD',
-    pic: '/assets/images/placeholder-transparent.png'
+    pic: '/assets/images/bookcovers/TheFamInvestigation.png',
+    release: 'January 31, 2025'
   },
   {
     title: 'The Bad Idea',
@@ -27,7 +29,8 @@ const books = [
             as she realizes how a  bad idea can affect her interactions with her family and how a simple good idea can rescue her
             her from the entangling effects of poor choices.`,
     link: 'Coming TBD',
-    pic: '/assets/images/placeholder-transparent.png'
+    pic: '/assets/images/bookcovers/TheBadIdea.png',
+    release: 'January 31, 2025'
   },
   {
     title: 'What is Our Baby',
@@ -35,7 +38,27 @@ const books = [
             start to wonder if their pregnant mother is anything like blind bags. Could she be carrying a fairy princess? Maybe a 
             playful puppy? Or perhaps even a bunch of chicken nuggets? `,
     link: 'Coming TBD',
-    pic: '/assets/images/placeholder-transparent.png'
+    pic: '/assets/images/bookcovers/WhatIsOurBaby.png',
+    release: 'January 31, 2025'
+  },
+  {
+    title: 'Mothers',
+    blurb: `Dive into an enchanting telling of three curious owl siblings. When their father brings home surprise toy bags, they 
+            start to wonder if their pregnant mother is anything like blind bags. Could she be carrying a fairy princess? Maybe a 
+            playful puppy? Or perhaps even a bunch of chicken nuggets? `,
+    link: 'Coming TBD',
+    pic: '/assets/images/bookcovers/Mothers.png',
+    release: 'April 20, 2025'
+
+  },
+  {
+    title: 'Fathers',
+    blurb: `Dive into an enchanting telling of three curious owl siblings. When their father brings home surprise toy bags, they 
+            start to wonder if their pregnant mother is anything like blind bags. Could she be carrying a fairy princess? Maybe a 
+            playful puppy? Or perhaps even a bunch of chicken nuggets? `,
+    link: 'Coming TBD',
+    pic: '/assets/images/bookcovers/Fathers.png',
+    release: 'April 20, 2025'
   }
 ];
 
@@ -44,20 +67,27 @@ const books = [
 let bookContainer = document.querySelector('.bookCont');
 let i = 0;
 books.forEach(book => {
-  
+  let link;
+  if (book.link != 'Coming TBD') { 
+    link = '<button class="bookButton"><a class="bookLink" href="#">${book.link}</a></button>';
+  } else {
+    link = `<div class="bookAvailCont"><h1 class="bookAvail">Available on Amazon <br>${book.release}</h1></div>`;
+  };
+
   const bookDiv = document.createElement('div');
   bookDiv.classList.add('book'+ [i]);
   bookDiv.innerHTML = `
     <div class=bookCont>
       <div class=bookWordCont>
+        <div class=bookImgCont>
+          <img class="bookImg" src="${book.pic}" alt="Book Image">   
+        </div>
         <h2 class="bookH">${book.title}</h2> 
         <p class="bookP">${book.blurb}</p> 
-        <button><a class="bookLink" href="#">${book.link}</a></button>
-      </div> 
-      <div class=bookImgCont>
-        <img class="bookImg" src="${book.pic}" alt="Book Image">   
-      </div>
+        ${link}
+        </div> 
     </div>  
+    <br>
   `;
   bookContainer.appendChild(bookDiv);
   i++;
