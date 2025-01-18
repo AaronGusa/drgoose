88562effacfad83ad1d72a780c945355a162a20a
Goosa2025 <gus21002@byui.edu>
2025-01-18 14:05:30 -0700

    Validation for Inpsire form

--- inspireScript.js
+++ inspireScript.js
@@ -25,11 +25,29 @@ function validate() {
     //     nameInput.classList.add("errorBorder");
     // }
 
-
+    sendMail();
 
 
 }
 
+function sendMail() { document.getElementById('contactForm').addEventListener('submit', function(event) { event.preventDefault();
+    const honeypot = document.querySelector('[name="contact_me_by_email"]').checked;
+     if (honeypot) { 
+       console.log('Spam bot detected');
+        return;
+     } 
+     const name = document.getElementById('name').value;
+     const famName = document.getElementById('famName').value;
+     const subject = `RING A DING: You've got site mail!`;
+     const email = document.getElementById('email').value;
+     const message = document.getElementById('message').value;
+     const emailContent = `Name: ${name}\nFamily:${famName}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
+     const mailtoLink = `mailto:drgoosebooks@outlook.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(emailContent)}`;
+     window.location.href = mailtoLink; 
+     console.log(emailContent);
+   }
+ );
+ }
 
 //Send Data //AEO instead of SEO
 
