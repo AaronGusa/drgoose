f68c2907003f4a7a85a6c313ff1614f2fd692b48
Goosa2025 <gus21002@byui.edu>
2025-01-27 11:41:56 -0700

    Hero cleaned and forms aligned

--- inspireScript.js
+++ inspireScript.js
@@ -41,7 +41,7 @@ function sendMail() { document.getElementById('contactForm').addEventListener('s
      const subject = `RING A DING: You've got site mail!`;
      const email = document.getElementById('email').value;
      const message = document.getElementById('message').value;
-     const emailContent = `Name: ${name}\nFamily:${famName}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}`;
+     const emailContent = `Name: ${name}\nFamily:${famName}\nEmail: ${email}\n\nStory Premise: ${message}`;
      const mailtoLink = `mailto:drgoosebooks@outlook.com?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(emailContent)}`;
      window.location.href = mailtoLink; 
      console.log(emailContent);
